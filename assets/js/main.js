d08d4767eee63516924528786e69081504a9a2c2
Saddy14 <1221301874@student.mmu.edu.my>
2025-09-19 21:53:36 +0800

    path fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,9 +36,10 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'../../images/bg01.jpg': 'center',
-							'../../images/bg02.jpg': 'center',
-							'../../images/bg03.jpg': 'center'						},
+							'images/bg01.jpg': 'center',
+							'images/bg02.jpg': 'center',
+							'images/bg03.jpg': 'center',
+						},
 
 					// Delay.
 						delay: 6000
